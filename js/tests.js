f86aa61d55fcb7206bc86171e066263ea1b4ff73
Artem Prokhorov <risonipli@gmail.com>
2015-03-03 16:37:46 +0300

    async tests

--- js/tests.js
+++ js/tests.js
@@ -1,7 +1,9 @@
 QUnit.test("asyncForEach test [1,2,3]", function (assert) {
+    var testDone = assert.async();
     cleanConsoleOutput();
     var array = [1, 2, 3];
     var aTimout = 1000;
+    var expected = '1\n2\n3\nend\n';
 
     asyncForeach(array, function(item, index, done) {
         setTimeout(function() {
@@ -11,13 +13,53 @@ QUnit.test("asyncForEach test [1,2,3]", function (assert) {
     }, function() {
         console.log('end');
     });
-    var expected = '1\n2\n3\nend\n'
 
     setTimeout(function() {
         assert.equal(expected, consoleOutput, "1 2 3 end");
+        testDone();
     }, aTimout + 150)
+});
 
+QUnit.test("asyncForEach test [5,4,3,2,1]", function (assert) {
+    var testDone = assert.async();
+    cleanConsoleOutput();
+    var array = [5,4,3,2,1];
+    var aTimout = 1000;
+    var expected = '5\n4\n3\n2\n1\nok\n';
 
+    asyncForeach(array, function(item, index, done) {
+        setTimeout(function() {
+            console.log(item);
+            done();
+        }, aTimout);
+    }, function() {
+        console.log('ok');
+    });
 
-    assert.ok(true)
+    setTimeout(function() {
+        assert.equal(expected, consoleOutput, "5 4 3 2 1 ok");
+        testDone();
+    }, aTimout + 150)
+});
+
+QUnit.test("asyncForEach test []", function (assert) {
+    var testDone = assert.async();
+    cleanConsoleOutput();
+    var array = [];
+    var aTimout = 1000;
+    var expected = '';
+
+    asyncForeach(array, function(item, index, done) {
+        setTimeout(function() {
+            console.log(item);
+            done();
+        }, aTimout);
+    }, function() {
+        console.log('ok');
+    });
+
+    setTimeout(function() {
+        assert.equal(expected, consoleOutput, "empty string");
+        testDone();
+    }, aTimout + 150)
 });
